f53e79a7bd273c099902c159006658957e0348ba
nico25m <nickmelzi105@gmail.com>
2025-12-03 18:40:35 +0100

    03122025

--- src/components/Aside-right.jsx
+++ src/components/Aside-right.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+
+
 function AsideRight() {
+  const [news, setNews] = useState([]);
+
+  async function getNews() {
+    try {
+      const response = await fetch(
+        "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=9Uno3dLRvjqjGCdGhqv7A4FycXdrHnHE"
+      );
+
+      const data = await response.json();
+
+      const articles = data.response.docs;
+
+      // Prendiamo solo le prime 20 per non far esplodere tutto
+      setNews(articles.slice(0, 20));
+    } catch (error) {
+      console.error("Errore durante il fetch:", error);
+    }
+  }
+  
   return (
     <div className="aside-right">
       <h1>Aside-right</h1>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,15 +5,19 @@ function Main() {
 
   async function getNews() {
     try {
-      const response = await fetch("/.netlify/functions/nyt");
+      const response = await fetch(
+        "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=9Uno3dLRvjqjGCdGhqv7A4FycXdrHnHE"
+      );
+
       const data = await response.json();
 
       const articles = data.response.docs;
-      setNews(articles.slice(0, 20));
 
+      // Prendiamo solo le prime 20 per non far esplodere tutto
+      setNews(articles.slice(0, 20));
     } catch (error) {
       console.error("Errore durante il fetch:", error);
-    } 
+    }
   }
 
   return (
@@ -27,11 +31,10 @@ function Main() {
           {news.map((article) => (
             <div className="news-item" key={article._id}>
               <h3>{article.headline.main}</h3>
-              <p>{article.abstract}</p>
+              <p>{article.multimedia.credit}</p> 
             </div>
           ))}
         </div>
-
       </div>
     </div>
   );
